e383d1335e75d58e637f61969ef65f89d5fc7473
samarishourya-ops <samarishourya@gmail.com>
2025-12-11 19:38:12 +0530

    Add files via upload

---
+++ MaximumThreeNumber.java
@@ -0,0 +1,29 @@
+public class MaximumThreeNumber {
+    public static void main (String[]args) {
+
+// Find out the maximum of  three number take number input from user 
+
+
+
+
+        int a = 10;
+        int b = 20;
+        int c = 15;
+
+        if(a>b) {
+            if (a>c) {
+                System.out.println("A is Greater");
+            } else {
+                System.out.println("C is Greater");
+            }
+        } else if(b>c) {
+            System.out.println("B is Greater");
+        } else { 
+            System.out.println("C is Greater");
+            }
+        }
+
+    }
+
+    
+
